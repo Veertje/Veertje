07b77113277072e9abfc49f11232561709bf1fd7
unknown <Vera@Vera-PC.(none)>
2012-03-19 19:41:22 +0100

    Hoi.php toegevoegd XD

---
+++ hoi.php
@@ -0,0 +1,11 @@
+<?php
+if(isset($_POST['naam'])) {
+	echo 'Hoi, '.$_POST['naam'];
+}
+else {
+?>
+<form action='' method='POST'>
+<input type='text' name='naam'>
+<input type='submit'>
+</form>
+<?php }
